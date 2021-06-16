f5f995f5b0a2206da6637cee574283a34c9b8afe
Jeffrey Bouter <jb@warpnet.nl>
2021-06-16 09:28:51 +0200

    Update tmux
    - Don't re-open windows
    
    Signed-off-by: Jeffrey Bouter <jb@warpnet.nl>

--- tmux.sh
+++ tmux.sh
@@ -2,5 +2,10 @@
 
 # custom function to start a new SSH session in a new tmux window
 function t () {
-  tmux new-window -n "$1" "ssh $1"
+  if [ "$(tmux list-windows | grep -c "$1")" -gt 0 ]; then
+    windownumber=$(tmux list-windows | grep "$1" | cut -d ':' -f1)
+    tmux select-window -t "$windownumber"
+  else
+    tmux new-window -n "$1" "ssh $1"
+  fi
 }
